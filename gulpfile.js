26f60dbf1bf47653843dc422f52ec067cb784a70
Yura <yurrianton@gmail.com>
2020-11-25 14:53:26 +0500

    test_gulp

---
+++ gulpfile.js
@@ -0,0 +1,77 @@
+const gulp             = require('gulp');
+const browserSync      = require('browser-sync');
+const sass             = require('gulp-sass');
+const rename           = require("gulp-rename");
+const autoprefixer     = require('gulp-autoprefixer');
+const cleanCSS         = require('gulp-clean-css');
+const imagemin         = require('gulp-imagemin');
+const imageminPngquant = require('imagemin-pngquant');
+const htmlmin          = require('gulp-htmlmin');
+
+gulp.task('server', function() {
+   browserSync.init({
+      server: {
+         baseDir: "dist"
+      }
+   });
+   gulp.watch('src/*.html').on("change", browserSync.reload);
+});
+
+gulp.task('styles', function() {
+   return gulp.src("src/scss/*.+(scss|sass)")
+      .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+      .pipe(rename({
+         prefix: "",
+         suffix: ".min",
+      }))
+      .pipe(autoprefixer({
+         overrideBrowserslist: ['last 3 versions'],
+         cascade: false
+      }))
+      .pipe(cleanCSS({compatibility: 'ie8'}))
+      .pipe(gulp.dest("dist/css"))
+      .pipe(browserSync.stream());
+});
+
+gulp.task('watch', function() {
+   gulp.watch("src/scss/*.+(scss|sass|css)", gulp.parallel("styles"));
+   gulp.watch('src/*.html').on("change", gulp.parallel('html'));
+});
+
+gulp.task('html', function() {
+   return gulp.src("src/*.html")
+      .pipe(htmlmin({collapseWhitespace: true}))
+      .pipe(gulp.dest("dist"));
+});
+
+gulp.task('script', function() {
+   return gulp.src("src/js/**/*.js")
+      .pipe(gulp.dest("dist/js"));
+});
+
+gulp.task('font', function() {
+   return gulp.src("src/fonts/**/*")
+      .pipe(gulp.dest("dist/fonts"));
+});
+
+gulp.task('icon', function() {
+   return gulp.src("src/icons/**/*")
+      .pipe(gulp.dest("dist/icons"));
+});
+
+gulp.task('images', function() {
+   return gulp.src("src/images/**/*")
+      .pipe(imagemin({
+         plugins: [
+            imageminPngquant()
+         ],
+         svgoPlugins: [{
+            removeViewBox: true,
+            cleanupIDs: false
+         }]
+      }))
+      .pipe(gulp.dest("dist/images"));
+});
+
+
+gulp.task('default', gulp.parallel('watch', 'server', 'styles', 'html', 'script', 'font', 'icon', 'images'));
